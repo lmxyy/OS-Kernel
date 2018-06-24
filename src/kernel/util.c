2f5fdd5cc0350cb127db3c11174b72b5de0ce655
Lmxyy <664789531@qq.com>
2018-06-24 14:37:57 +0800

    finish util

---
+++ src/kernel/util.c
@@ -0,0 +1,21 @@
+void memoryCopy(char *source,char *dest,int nbytes)
+{
+    int i = 0;
+    for (i = 0;i < nbytes;++i)
+	*(dest+i) = *(source+i);
+}
+
+void int2Ascii(int n,char str[])
+{
+    int i = 0,j = 0,sign = 0;
+    if (n < 0) sign = 1,n = -n;
+    do str[i++] = n%10+'0',n /= 10;
+    while (n);
+    if (sign) str[i++] = '-';
+    for (j = 0;j < (i-j-1);++j)
+    {
+	char c = str[j];
+	str[j] = str[i-j-1];
+	str[i-j-1] = c;
+    }
+}
---
+++ src/kernel/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void memoryCopy(char *source,char *dest,int nbytes);
+void int2Ascii(int n,char str[]);
+
+#endif
